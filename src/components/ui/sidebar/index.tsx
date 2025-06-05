429701b39ed799b99daa81c2225367f157d30b9f
khalilDsouza <khalil@qriarlabs.com>
2025-06-05 15:28:30 -0300

    update enviroment

--- src/components/ui/sidebar/index.tsx
+++ src/components/ui/sidebar/index.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { final, homeMenu } from "./menu";
-import { SidebarWrapper } from "@qriar-labs/qore";
-import Link from "next/link";
-import Image from "next/image";
+
 
 interface SidebarProps {
   children: React.ReactNode;
