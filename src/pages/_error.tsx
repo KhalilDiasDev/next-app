46a74cabec30917bcb092f26da5f2b6b222f7a57
khalilDsouza <khalil@qriarlabs.com>
2025-06-27 15:29:41 -0300

    fix 404 page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Layout } from "antd";
+import { Button } from "antd";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,38 +7,29 @@ export default function ErrorPage() {
   return (
     <>
       <Head>
-        <title>QAP Canvas not-found</title>
+        <title>QAP Canvas - Página não encontrada</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className="d-flex flex-column align-items-center justify-content-center text-center mt-10">
-        <div>
+
+      <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-black via-gray-900 to-black text-white px-4 relative">
+        <div className="mb-8">
           <Image
-            src={"/404-error.png"}
+            src="/404-error.png"
             width={400}
             height={400}
-            alt="NOT_FOUND_IMAGE"
-            className="img-fluid rounded-2"
+            alt="Imagem de erro 404"
+            className="rounded-2xl"
           />
         </div>
-        <h2>Page not found</h2>
+
+        <h2 className="text-2xl font-bold mb-4">Página não encontrada</h2>
+
         <Link href="/home">
-          <Button className="mt-5 p-2" type="primary" size="large">
-            Return to home
-          </Button>
+          <button className="mt-4 px-6 py-2 flex-1 bg-blue-500 border-blue-500 text-white hover:bg-blue-600 hover:text-white rounded">
+            Voltar para o início
+          </button>
         </Link>
 
-        <Image
-          src={"/logo.png"}
-          width={187}
-          height={42}
-          className="object-contain"
-          alt="Corner Image"
-          style={{
-            position: "absolute",
-            top: 30,
-            right: 30,
-          }}
-        />
       </main>
     </>
   );
